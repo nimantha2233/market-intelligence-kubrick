4dc89fcbebbefc7069ed1d28c36a45d3c9785394
Arian Khan <ariankhankhan@kubrickgroup.com>
2024-05-17 15:33:40 +0100

    obtain yahoo finance code

--- arian-scripts/temp.py
+++ arian-scripts/temp.py
@@ -1,8 +1,10 @@
 import pandas as pd
-from SupportFunctions import sheet_exists, write_to_excel, compare_rows, read_template
-
+from SupportFunctions import sheet_exists, write_to_excel, compare_rows, read_template, get_company_status, get_company_details
+import yfinance
 # Define the data
 
 df = read_template()
 
-print(df)
+symbol = "FDM.L"
+
+print(get_company_details(symbol))
