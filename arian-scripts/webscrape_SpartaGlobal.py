d0e8e83dae897d90ef0b2269e5432f719ad21bb1
Arian Khan <ariankhankhan@kubrickgroup.com>
2024-05-22 11:29:13 +0100

    SpartGlobal script finished

--- arian-scripts/webscrape_SpartaGlobal.py
+++ arian-scripts/webscrape_SpartaGlobal.py
@@ -1,52 +1,48 @@
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
 import os
-from SupportFunctions import sheet_exists, write_to_excel, compare_rows
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from collections import defaultdict
+from SupportFunctions import write_to_excel, read_from_excel, get_company_details, log_new_and_modified_rows, create_final_df
 
-url = "https://www.spartaglobal.com/careers/"
-expertise_dict = {'Practices': [], 'Expertise_url': [], 'Expertise': []}
-temp_dict = {'Practices': [], 'Expertise_url': []}
-r = requests.get(url)
 
-# Parse the HTML using BeautifulSoup
-soup = BeautifulSoup(r.text, 'lxml') # Ensure lxml is isntalled via pip
+def main():
+    company_dict = defaultdict(list)
+    temp_dict = defaultdict(list)
+    url = "https://www.spartaglobal.com/careers/"
+    company_longname = "" # SpartaGlobal is a private company - so no ticker symbol for this.
+    file_path = "Kubrick MI Data.xlsx"
+    
 
-# Find all div elements with specified classes
-div_elements = soup.find_all('div', class_=lambda x: x and 'item' in x.split() and 'slick-cloned' not in x.split())
+    r = requests.get(url)
 
-# Example: Extract href and process to get desired text
-for div in div_elements:
-    a_tag = div.find('a')
-    if a_tag:
-        href = a_tag.get('href')
-        expertise_name = href.rsplit('/', 2)[-2].replace('-', ' ').title()
-        full_url = f"https://www.spartaglobal.com{href}"
-        expertise_dict['Practices'].append(expertise_name)
-        expertise_dict['Expertise_url'].append(full_url)
-        expertise_dict['Expertise'].append('')
+    # Parse the HTML using BeautifulSoup
+    soup = BeautifulSoup(r.text, 'lxml') # Ensure lxml is isntalled via pip
 
-# Convert dictionary to DataFrame
-df = pd.DataFrame(expertise_dict)
+    # Find all div elements with specified classes
+    div_elements = soup.find_all('div', class_=lambda x: x and 'item' in x.split() and 'slick-cloned' not in x.split())
 
-# File path remains the same
-file_path = "Kubrick MI Data.xlsx"
+    # Example: Extract href and process to get desired text
+    for div in div_elements:
+        a_tag = div.find('a')
+        if a_tag:
+            href = a_tag.get('href')
+            expertise_name = href.rsplit('/', 2)[-2].replace('-', ' ').title()
+            full_url = f"https://www.spartaglobal.com{href}"
+            company_dict['Practices'].append(expertise_name)
+            company_dict['Practices_URL'].append(full_url)
 
-# Derive sheet_name from the script name
-script_name = os.path.basename(__file__)
-# Extract the part after "webscrape_" to use as the sheet name
-sheet_name = script_name.split('webscrape_')[-1].split('.')[0]
+    # Convert dictionary to DataFrame
+    df = pd.DataFrame(company_dict)
 
-# Check if the Excel file exists
-if not os.path.exists(file_path):
-    # If the file doesn't exist, create a new Excel file with the DataFrame
-    write_to_excel(df, file_path, sheet_name)
-    print(f"New Excel file '{file_path}' created with '{sheet_name}' sheet.")
-else:
-    # If the file exists, check if the sheet exists and compare rows
-    if not sheet_exists(file_path, sheet_name) or compare_rows(df, file_path, sheet_name):
-        # If the sheet doesn't exist or the number of rows is different, write to Excel
-        write_to_excel(df, file_path, sheet_name)
-        print(f"Data written to '{sheet_name}' sheet in '{file_path}'.")
-    else:
-        print(f"No changes observed for '{sheet_name}' sheet in '{file_path}'.")
+    # Block of code with functions from SupportFunctions to save data and create diff report
+    script_name = os.path.basename(__file__)
+    sheet_name = script_name.split('webscrape_')[-1].split('.')[0]
+    financial_json = get_company_details(company_longname) # Obtains yfinance data
+    company_df = create_final_df(sheet_name, url, financial_json, df)
+    old_df = read_from_excel(file_path, sheet_name) # Obtains old records
+    log_new_and_modified_rows(company_df, old_df, sheet_name) # Creates a df with differences
+    write_to_excel(company_df, file_path, sheet_name)
+
+if __name__ == "__main__":
+    main()
